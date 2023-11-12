6c00f0ab2ccf066202c6ab5eb026f85521bcc477
Anand Kumar Maurya <141118620+AjayKumarMaurya1@users.noreply.github.com>
2023-11-11 19:16:38 -0800

    chang thim

---
+++ riyans.js
@@ -0,0 +1,7 @@
+document.querySelector(".searchbtn").onclick = function () {
+    document.querySelector("#searchbox").classList.toggle("show");
+};
+
+document.querySelector("#navhembirg").onclick = function () {
+    document.querySelector("nav").classList.toggle("show");
+};
